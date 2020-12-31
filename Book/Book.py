f8b754cf2293698d41fe7e83b3ff5f986200bb65
arshianrz <arshia.nozarnezhad@gmail.com>
2020-12-31 17:59:25 +0330

    Dec 31 commit

--- Book/Book.py
+++ Book/Book.py
@@ -1,58 +1,69 @@
+import json
+
 class Book:
     """
         [we should be able to create and delete book objects for our library.]
     """
+    ID_counter = 1
     books_quantity = 0
-    def __init__(self,name,writer,genre,price,quantity):
+
+    def __init__(self, name, author, genre, price, quantity, ID):
         """
         [constructor]
 
         Args:
             name ([string]): [book title]
-            writer ([string]): [write of the book]
+            author ([string]): [writer of the book]
             genre ([string]): [book's genre]
             quantity ([integer]): [number of specific book]
             price ([integer]): [book's price]
         """
         self.name = name
-        self.writer = writer
+        self.author = author
         self.genre = genre
         self.quantity = quantity
         self.price = price
+        self.ID = ID
+        Book.ID_counter += 1
         Book.books_quantity += self.quantity
-    
+
     def book_info(self):
         """
         Prints a general information about a specific book.
         """
-        print("Name : ",self.name,
-              "Writer : ",self.writer,
-              "Book's Genre : ",self.genre,
-              "Quantity of this book : ",self.quantity)
+        print("ID : ",                      self.ID,
+              "\nName : ",                  self.name,
+              "\nAuthor : ",                self.author,
+              "\nGenre : ",                 self.genre,
+              "\nPrice : ",                 self.price,
+              "\nQuantity of this book : ", self.quantity)
 
-    def set_name(self,name):
-        Book.name = name
-    def set_writer(self,writer):
-        Book.writer = writer
-    def set_genre(self,genre):
-        Book.genre = genre
-    def set_quantity(self,bookQuantity):
-        Book.bookQuantity = bookQuantity
-    def set_price(self,price):
-        Book.price = price
- 
- 
-    def get_name(self):
-        return self.name 
-    def get_writer(self):
-        return self.writer
-    def get_genre(self):
-        return self.genre
-    def get_quantity(self):
-        return self.quantity
-    def get_price(self):
-        return self.price
-    
-    @staticmethod
-    def get_books_quantity():
-        return Book.books_quantity
+    def create_book(self):
+        print("You are about to create a book ,please answer following questions.")
+        self.name = input("Enter title :")
+        self.author = input("Enter author :")
+        self.genre = input("Enter genre :")
+        self.price = input("Enter price :")
+        self.quantity = input("Enter quantity :")
+        self.ID = 1000 + self.ID_counter
+        serialized_object = json.dumps(self.__dict__)
+        book_file = open("Books.txt", "a")
+        book_file.write(serialized_object)
+        book_file.close()
+
+    def remove_book(self):
+        check = str(input("You are about to remove a book. \n Are you sure (Y/N) ?")).lower().strip()
+        try:
+            if check[0] == 'y':
+                del self
+                print("Book deleted successfully.")
+            elif check[0] == 'n':
+                print("Removing operation cancelled.")
+                return False
+            else:
+                print('Invalid Input')
+                return self.remove_book(self)
+        except Exception as error:
+            print("Please enter valid inputs")
+            print(error)
+            return self.remove_book(self)
--- Customer/Customer.py
+++ Customer/Customer.py
@@ -1,7 +1,9 @@
-class Customer:
-    """
-    
-    """
-    def __init__(self,name,lastName):
+from BookStore import Book
+
+class Customer(Book):
+    def __init__(self,name,last_name,purchase_date,book_choice):
         self.name = name
-        self.lastName = lastName
+        self.last_name = last_name
+        self.purchase_date = purchase_date
+        self.book_choice = book_choice
+        
